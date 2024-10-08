b1c0aac49e8881bccab6f5d773db49afb985838a
1abcdesign <one.abcdesign@gmail.com>
2024-10-08 12:41:53 +0300

    Updates

---
+++ assets/Design-CTYyDF2B.js
@@ -0,0 +1,1 @@
+import{_ as n,c as t,t as s,o as c}from"./index-BUbiFjz3.js";const o={};function r(e,a){return c(),t("div",null,s(e.$t("design")),1)}const _=n(o,[["render",r]]);export{_ as default};
