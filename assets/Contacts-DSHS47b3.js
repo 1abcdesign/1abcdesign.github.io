2252409a41ccf3d1da173e7ddb4b83d52d02ec6b
1abcdesign <one.abcdesign@gmail.com>
2024-10-12 14:18:40 +0300

    Updates

--- assets/Contacts-DSHS47b3.js
+++
@@ -1,1 +0,0 @@
-import{_ as a,c as o,e as n,t as r,h as s,a as c}from"./index-3q79ctRU.js";const l={};function f(t,e){return c(),o("div",null,[n(r(t.$t("contact_phone"))+": ",1),e[0]||(e[0]=s("a",{href:"tel:+380930370327"}," +38(093)037-03-27 ",-1))])}const d=a(l,[["render",f]]);export{d as default};
--- assets/Design-CndVsc1K.js
+++
@@ -1,1 +0,0 @@
-import{_ as n,c as t,t as s,a as c}from"./index-3q79ctRU.js";const r={};function a(e,o){return c(),t("div",null,s(e.$t("design")),1)}const _=n(r,[["render",a]]);export{_ as default};
