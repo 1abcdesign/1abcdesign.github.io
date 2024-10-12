568da20f46fa252a502bbb18e35e1fe5be00b756
1abcdesign <one.abcdesign@gmail.com>
2024-10-12 19:14:50 +0300

    Updates

---
+++ assets/Building-BS9blAys.js
@@ -0,0 +1,1 @@
+import{_ as n,c as t,t as c,a as r}from"./index-5ZaseCFg.js";const a={};function o(e,s){return r(),t("div",null,c(e.$t("building")),1)}const l=n(a,[["render",o]]);export{l as default};
