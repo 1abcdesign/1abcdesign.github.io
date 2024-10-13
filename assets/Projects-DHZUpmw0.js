df39f7f1143a4f1e22f26688a2c4ae3896c24a04
1abcdesign <one.abcdesign@gmail.com>
2024-10-13 13:11:21 +0300

    Updates

---
+++ assets/Projects-DHZUpmw0.js
@@ -0,0 +1,1 @@
+import{_ as s,c as t,t as c,a as o}from"./index-ubC8rgxj.js";const r={};function a(e,n){return o(),t("div",null,c(e.$t("showcases")),1)}const l=s(r,[["render",a]]);export{l as default};
