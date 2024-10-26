a00caf884f2e242bab920522216d8732817e6bc4
1abcdesign <one.abcdesign@gmail.com>
2024-10-26 04:36:34 +0300

    assets preoad + contacts i18n

--- src/views/Contacts.vue
+++ src/views/Contacts.vue
@@ -1,12 +1,16 @@
 <template>
   <main>
     {{ $t('contact_phone') }}:
-    <a href="tel:+380930370327">
-      +38(093)037-03-27
+    <a href="tel:+380933789883">
+      +380 933 789 883
     </a>
+    <br>
+    {{ $t('contact_tg') }}:
     <a href="http://t.me/abcdesign1">
       @abcdesign1
     </a>
+    <br>
+    {{ $t('contact_mail') }}:
     <a href="mailto:one.abcdesign@gmail.com">
       one.abcdesign@gmail.com
     </a>
