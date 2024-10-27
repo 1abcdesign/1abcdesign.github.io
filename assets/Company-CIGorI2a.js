e7229be52b4e04a7403780063a1b763b16e3dfcc
1abcdesign <one.abcdesign@gmail.com>
2024-10-27 12:15:16 +0200

    Updates

---
+++ assets/Company-CIGorI2a.js
@@ -0,0 +1,1 @@
+import{_ as a,c as e,t as n,a as o}from"./index-lTmdFOCw.js";const c={};function r(t,s){return o(),e("main",null,n(t.$t("about")),1)}const l=a(c,[["render",r]]);export{l as default};
