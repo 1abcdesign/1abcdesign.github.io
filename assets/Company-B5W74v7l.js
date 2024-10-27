3384d2a080675f2b89a56a02b4da1dd7cbdc4e42
1abcdesign <one.abcdesign@gmail.com>
2024-10-27 14:55:19 +0200

    Updates

--- assets/Company-B5W74v7l.js
+++
@@ -1,1 +0,0 @@
-import{_ as a,c as e,t as n,a as o}from"./index-B7pKSOGo.js";const c={};function r(t,s){return o(),e("main",null,n(t.$t("about")),1)}const l=a(c,[["render",r]]);export{l as default};
