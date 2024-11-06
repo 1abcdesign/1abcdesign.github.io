9e5ffc0c1bf5cd2a7ca3f997d374fe3d40b7db78
1abcdesign <one.abcdesign@gmail.com>
2024-11-06 10:23:51 +0200

    company: .heart color to red

--- src/views/Company.vue
+++ src/views/Company.vue
@@ -1,11 +1,11 @@
 <template>
-  <main>
+  <main class="company">
     <p>
       {{ $t('aboutUs1') }}
     </p>
 
-    <p>
-      {{ $t('aboutUs2') }}
+    <p class="with-love">
+      {{ $t('aboutUs21') }}<span class="heart">❤</span>{{ $t('aboutUs22') }}
     </p>
 
     <section class="vip">
@@ -48,24 +48,6 @@
     <router-link to="/contacts">
       {{ $t('contacts') }}
 
-      <!-- <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="24"
-        height="24"
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="currentColor"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        class="icon feather-user-plus"
-      >
-        <path d="M16 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path>
-        <circle cx="8.5" cy="7" r="4"></circle>
-        <line x1="20" y1="8" x2="20" y2="14"></line>
-        <line x1="23" y1="11" x2="17" y2="11"></line>
-      </svg> -->
-
       <svg
         xmlns="http://www.w3.org/2000/svg"
         width="24"
@@ -85,8 +67,8 @@
 
 <script setup></script>
 
-<style lang="scss" scoped>
-main {
+<style lang="scss">
+main.company {
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -177,5 +159,9 @@ main {
     height: 1.5rem;
     width: 1.5rem;
   }
+}
+
+.heart {
+  color: red !important;
 }
 </style>
