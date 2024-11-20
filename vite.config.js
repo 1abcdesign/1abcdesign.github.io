16a4200ad4312451d9925828fa613d04dec892d5
1abcdesign <one.abcdesign@gmail.com>
2024-11-20 18:44:54 +0200

    optimize via vite.config

--- vite.config.js
+++ vite.config.js
@@ -23,5 +23,28 @@ export default defineConfig(({ command, mode }) => {
         },
       },
     },
+    build: {
+      rollupOptions: {
+        output: {
+          // Group dependencies into separate chunks
+          manualChunks: {
+            vue: ['vue', 'vue-router', 'vue-i18n'],
+            three: ['three'],
+            vendor: ['axios', 'emailjs-com', 'vueperslides', 'vue3-google-autocomplete'],
+          },
+        },
+      },
+      chunkSizeWarningLimit: 600, // Increase limit to reduce warnings for large chunks
+      assetsDir: 'assets', // Store static assets (JS, CSS, etc.) in a dedicated folder
+      manifest: true, // Generates a manifest file to map hashed assets
+      cssCodeSplit: true, // Split CSS into separate files for better caching
+      sourcemap: false, // Disable sourcemaps in production for smaller build size
+      minify: 'esbuild', // Use esbuild for faster builds
+    },
+    optimizeDeps: {
+      // Prebundle dependencies for faster development builds
+      include: ['axios', 'three', 'vue', 'vue-router', 'vue-i18n', 'vue3-google-autocomplete'],
+      exclude: [], // If you have large dependencies you want to avoid prebundling, add them here
+    },
   }
 })
